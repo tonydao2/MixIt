3284a5a6b3b2d2b0b90ef27640f009e767b05b97
Tony Dao <tonydao@tonys-mbp-2.myfiosgateway.com>
2024-07-24 18:10:09 -0400

    fixed styling

--- app/home/TracksRemix.tsx
+++ app/home/TracksRemix.tsx
@@ -81,48 +81,59 @@ export default function TracksRemix({ tracks, accessToken }: TracksRemixProps) {
   // TODO: Include the picture of the playlist of the song in response
 
   return (
-    <div className='justify-between flex flex-row w-full p-10 items-center'>
+    <div className='flex flex-col justify-center w-full p-10'>
       {tracks.length > 0 && (
         <>
-          <div className='flex flex-col w-full'>
-            {tracks.map((track) => (
-              <div
-                key={track.track.id}
-                className='flex items-center bg-blue-500 p-4 mb-4 rounded w-4/5 h-48'
-              >
-                <Image
-                  loader={myLoader}
-                  src={track.track.album.images[0].url}
-                  alt={track.track.name}
-                  width={150}
-                  height={150}
-                />
-                <p className='ml-10'>{track.track.name}</p>
-              </div>
-            ))}
-          </div>
-          {/* TODO: Use MUI for Select prop component */}
-          <div className='flex flex-col w-full'>
-            {remixes.map((trackRemix) => (
-              <div
-                key={trackRemix.track.id}
-                className='flex items-center bg-blue-500 p-4 mb-4 rounded w-4/5 h-48'
-              >
-                {trackRemix.remixes.length > 0 ? (
-                  <select className='ml-4 p-2 rounded'>
-                    {trackRemix.remixes.map((remix) => (
-                      <option key={remix.id} value={remix.id}>
-                        {remix.name}
-                      </option>
-                      // TODO: Add plus sign here to add remix to add to list to call add to playlist
-                      // Then change to minus sign to remove from list
-                    ))}
-                  </select>
-                ) : (
-                  <p className='text-white ml-4'>No remixes found</p>
-                )}
-              </div>
-            ))}
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-col w-3/5 mr-2'>
+              {tracks.map((track) => (
+                <div
+                  key={track.track.id}
+                  className='flex items-center bg-blue-500 p-4 mb-4 rounded flex-grow'
+                >
+                  <Image
+                    loader={myLoader}
+                    src={track.track.album.images[0].url}
+                    alt={track.track.name}
+                    width={150}
+                    height={150}
+                  />
+                  <div className='ml-10 text-white text-left'>
+                    <p>{track.track.name}</p>
+                    <p>
+                      {track.track.artists
+                        .map((artist) => artist.name)
+                        .join(', ')}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {/* TODO: Use MUI for Select prop component */}
+            <div className='flex flex-col items-center w-3/5 ml-2'>
+              {remixes.map((trackRemix) => (
+                <div
+                  key={trackRemix.track.id}
+                  className='flex items-center bg-blue-500 p-4 mb-4 rounded w-full h-48'
+                >
+                  {trackRemix.remixes.length > 0 ? (
+                    <select className='ml-4 p-2 rounded'>
+                      {trackRemix.remixes.map((remix) => (
+                        <option key={remix.id} value={remix.id}>
+                          {remix.name}
+                        </option>
+                        // TODO: Add plus sign here to add remix to add to list to call add to playlist
+                        // Then change to minus sign to remove from list
+                      ))}
+                    </select>
+                  ) : (
+                    <p className='text-white ml-4'>
+                      No alternate versions found
+                    </p>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
           {/* <Button onClick={addPlaylist}>Add remixes to playlist</Button> */}
         </>
