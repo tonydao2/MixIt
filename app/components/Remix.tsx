ea6529c1ee43972f221fc4fff0c526a1980a3599
Tony Dao <tonydao1@bu.edu>
2024-07-28 18:41:37 -0400

    working on rebase to add Tracks to remix

--- app/components/Remix.tsx
+++ app/components/Remix.tsx
@@ -11,17 +11,21 @@ import {
 import Image from 'next/image';
 import Button from './Button';
 
+interface TrackRemix {
+  track: Track['track'];
+  remixes: RemixTracks[];
+}
+
 interface RemixProps {
-  trackRemix: {
-    track: Track['track'];
-    remixes: RemixTracks[];
-  };
+  trackRemix: RemixTracks[];
+  tracks: Track[];
   accessToken: string | undefined;
   playlistId: string | undefined;
 }
 
 export default function Remix({
   trackRemix,
+  tracks,
   accessToken,
   playlistId,
 }: RemixProps) {
@@ -29,12 +33,11 @@ export default function Remix({
   const [isInPlaylist, setIsInPlaylist] = useState(false);
 
   // Check if the selected remix is already in the playlist
-  console.log(setSelectedRemix);
+  console.log('trackRemix', trackRemix);
 
   const handleChange = async (event: SelectChangeEvent<string>) => {
     const remixId = event.target.value;
-    const selected =
-      trackRemix.remixes.find((remix) => remix.id === remixId) || null;
+    const selected = trackRemix.find((remix) => remix.id === remixId) || null;
     setSelectedRemix(selected);
 
     if (selected && accessToken && playlistId) {
@@ -103,101 +106,101 @@ export default function Remix({
   }
 
   return (
-    <div
-      key={trackRemix.track.id}
-      className='flex items-center p-4 mb-4 rounded w-full h-48 '
-    >
-      {trackRemix.remixes.length > 0 ? (
-        <div className='flex flex-row items-center'>
-          <div className='flex-shrink-0'>
-            {selectedRemix && (
-              <Image
-                loader={myLoader}
-                src={selectedRemix.album.images[0].url || ''}
-                alt={trackRemix.track.name}
-                width={150}
-                height={150}
-              />
-            )}
-          </div>
-          <div className='ml-4'>
-            <FormControl variant='standard' className='ml-4 max-w-96'>
-              <InputLabel
-                id={`select-remix-label-${trackRemix.track.id}`}
-                sx={{
-                  color: 'white',
-                  marginBottom: '0.5rem',
-                }}
-              >
-                Select Remix
-              </InputLabel>
-              <Select
-                labelId={`select-remix-label-${trackRemix.track.id}`}
-                id={`select-remix-${trackRemix.track.id}`}
-                value={selectedRemix?.id || ''}
-                onChange={handleChange}
-                label='Select Remix'
-                sx={{
-                  color: 'white',
-                  '.MuiOutlinedInput-notchedOutline': {
-                    borderColor: 'white',
-                  },
-                  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                    borderColor: 'white',
-                  },
-                  '.MuiSvgIcon-root ': {
-                    fill: 'white !important',
-                  },
-                  '.MuiSelect-select': {
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: 'flex',
-                    alignItems: 'center',
-                    fontStyle: 'italic',
-                  },
-                }}
-              >
-                <MenuItem value=''>
-                  <em>None</em>
-                </MenuItem>
-                {trackRemix.remixes.map((remix) => (
-                  <MenuItem
-                    key={remix.id}
-                    value={remix.id}
-                    style={{
-                      maxWidth: '300px',
+    <div className='flex items-center p-4 mb-4 rounded w-full h-48 '>
+      {trackRemix.length > 0 ? (
+        trackRemix.map((remix: any) => (
+          <div className='flex flex-row items-center' key={remix.id}>
+            <div className='flex-shrink-0'>
+              {selectedRemix && (
+                <Image
+                  loader={myLoader}
+                  src={selectedRemix.album.images[0].url || ''}
+                  alt={remix.name}
+                  width={150}
+                  height={150}
+                />
+              )}
+            </div>
+            <div className='ml-4'>
+              <h5 className='font-semibold text-lg text-white'>{remix.name}</h5>
+              <FormControl variant='standard' className='ml-4 max-w-96'>
+                <InputLabel
+                  id={`select-remix-label-${remix.id}`}
+                  sx={{
+                    color: 'white',
+                    marginBottom: '0.5rem',
+                  }}
+                >
+                  Select Remix
+                </InputLabel>
+                <Select
+                  labelId={`select-remix-label-${remix.id}`}
+                  id={`select-remix-${remix.id}`}
+                  value={selectedRemix?.id || ''}
+                  onChange={handleChange}
+                  label='Select Remix'
+                  sx={{
+                    color: 'white',
+                    '.MuiOutlinedInput-notchedOutline': {
+                      borderColor: 'white',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                      borderColor: 'white',
+                    },
+                    '.MuiSvgIcon-root ': {
+                      fill: 'white !important',
+                    },
+                    '.MuiSelect-select': {
                       whiteSpace: 'nowrap',
                       overflow: 'hidden',
                       textOverflow: 'ellipsis',
-                    }}
-                  >
-                    <div className='flex items-center justify-between'>
-                      {remix.name}
-                    </div>
+                      display: 'flex',
+                      alignItems: 'center',
+                      fontStyle: 'italic',
+                    },
+                  }}
+                >
+                  <MenuItem value=''>
+                    <em>None</em>
                   </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            {selectedRemix && (
-              <div className='text-white text-left max-w-xs truncate'>
-                <h5 className='text-gray-400 text-sm font-medium italic truncate'>
-                  {selectedRemix.artists
-                    .map((artist) => artist.name)
-                    .join(', ')}
-                </h5>
-                {/* If song is in playlist change button to remove else add */}
-                {isInPlaylist ? (
-                  <Button onClick={removeFromPlaylist}>
-                    Remove from Playlist
-                  </Button>
-                ) : (
-                  <Button onClick={addToPlaylist}>Add to Playlist</Button>
-                )}
-              </div>
-            )}
+                  {trackRemix.map((remix: any) => (
+                    <MenuItem
+                      key={remix.id}
+                      value={remix.id}
+                      style={{
+                        maxWidth: '300px',
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                      }}
+                    >
+                      <div className='flex items-center justify-between'>
+                        {remix.name}
+                      </div>
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {selectedRemix && (
+                <div className='text-white text-left max-w-xs truncate'>
+                  <h5 className='text-gray-400 text-sm font-medium italic truncate'>
+                    {selectedRemix.artists
+                      .map((artist) => artist.name)
+                      .join(', ')}
+                  </h5>
+                  {/* If song is in playlist change button to remove else add */}
+                  {isInPlaylist ? (
+                    <Button onClick={removeFromPlaylist}>
+                      Remove from Playlist
+                    </Button>
+                  ) : (
+                    <Button onClick={addToPlaylist}>Add to Playlist</Button>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        ))
       ) : (
         <p className='text-white ml-4'>No alternate versions found</p>
       )}
